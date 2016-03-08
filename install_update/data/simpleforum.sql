4697de4ecc5cef836ce449cdaae3d23800bf23b9
Simple Forum <flyyjd@gmail.com>
2016-03-08 21:35:10 +0900

    bug fix

--- install_update/data/simpleforum.sql
+++ install_update/data/simpleforum.sql
@@ -223,7 +223,7 @@ INSERT INTO simple_setting(`sortid`, `block`, `label`, `type`, `key`, `value_typ
 (5,'extend', '编辑器', 'select','editor','text', 'wysibb', '普通论坛推荐Wysibb编辑器(BBCode)，技术类论坛推荐SimpleMarkdown编辑器。注意：换编辑器可能会使以前发的帖子格式混乱。', '{"wysibb":"Wysibb编辑器(BBCode)","smd":"SimpleMarkdown编辑器"}'),
 (1,'cache', '开启缓存', 'select','cache_enabled','integer', '0', '默认0（不开启）', '["0(关闭)","1(开启)"]'),
 (2,'cache', '缓存时间(分)', 'text','cache_time','integer', '10', '默认10分', ''),
-(3,'cache', '缓存类型', 'select','cache_type', 'text', 'file', '默认file', '{"file":"file","apc":"apc","memcache":"memcache 或 memcached"}'),
+(3,'cache', '缓存类型', 'select','cache_type', 'text', 'file', '默认file', '{"file":"file","apc":"apc","memcache":"memcache","memcached":"memcached"}'),
 (4,'cache', '缓存服务器', 'textarea','cache_servers', 'text','', '缓存类型设为MemCache时设置<br/>一个服务器一行，格式为：IP 端口 权重<br />示例：<br />127.0.0.1 11211 100<br />127.0.0.2 11211 200', ''),
 (1,'auth', '开启第三方登录', 'select','auth_enabled', 'integer','0', '', '["0(关闭)","1(开启)"]'),
 (1,'auth.qq', 'appid', 'text','qq_appid', 'text','', '', ''),
