1bfd9719538690e14d7035952c014bf89e77a3f4
unknown <flyyjd@gmail.com>
2021-01-02 13:07:16 +0900

    delete unnecessary files

--- core/vendor/bower-asset/inputmask/js/global/window.js
+++
@@ -1,6 +0,0 @@
-if (typeof define === "function" && define.amd)
-	define(function () {
-		return window;
-	});
-else if (typeof exports === "object")
-	module.exports = window;
--- core/vendor/yiisoft/yii2/.github/PULL_REQUEST_TEMPLATE.md
+++
@@ -1,3 +0,0 @@
-<!--
-Please use https://github.com/yiisoft/yii2 to report issues and send pull requests. Thank you!
--->
