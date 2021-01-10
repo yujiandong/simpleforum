f9331ec7ad2f04407eeeb557530a443369e7706f
Simple Forum <flyyjd@gmail.com>
2021-01-10 11:14:31 +0900

    bug fix

--- core/views/admin/user/charge.php
+++ core/views/admin/user/charge.php
@@ -45,7 +45,7 @@ if ( $session->hasFlash('ChargePointNG') ) {
             <?php echo $form->field($model, 'msg')->textarea(['rows' => 3, 'maxlength'=>255]); ?>
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-9">
-                <?php echo Html::submitButton(Yii::t('app/admin', 'Submit'), ['class' => 'btn btn-primary']); ?>
+                <?php echo Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']); ?>
                 </div>
             </div>
         <?php ActiveForm::end(); ?>
